4090ef0ea709763f03cc81a1b1558897b55f4ea2
ncsuddut <ncsuddut@ncsu.edu>
2024-01-01 01:05:45 -0500

    more visual improvements

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -88,9 +88,6 @@ export default class App extends React.Component<AppProps, AppState> {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
             <a
               className="App-link"
               href="https://reactjs.org"
